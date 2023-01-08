b50b20e5f9d25d7f5dde5a14a023446c158062b7
ken <ynkn0829@gmail.com>
2023-01-08 16:11:24 +0900

    parse_toml用のカスタムエラーを作成

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,11 @@
 #[derive(thiserror::Error, Debug)]
 pub enum CustomError {
+    #[error("アクセス確認先のURLが指定されていません。")]
+    AccessUrlNotDefined,
+
+    #[error("アクセス確認先のセレクタが指定されていません。")]
+    FindSelectorNotDefined,
+
     #[error("指定したURLにアクセスできませんでした。")]
     AccessUrlError,
 
